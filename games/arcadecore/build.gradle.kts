c36c82c9cb80687987f881a2785114a9a663cb9d
Yamo <diahnet@naver.com>
2026-09-08 14:43:24 +0900

    Add arcade core module

---
+++ games/arcadecore/build.gradle.kts
@@ -0,0 +1,21 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "com.yamone.games.arcadecore"
+    compileSdk = 36
+
+    defaultConfig {
+        minSdk = 26
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.17.0")
+}
