906189fbca231a86e2da6d3e5ba8c5c8581df292
Yamo <diahnet@naver.com>
2026-09-08 12:38:54 +0900

    Release Yamone Games Sudoku 1.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yamone.games"
         minSdk = 26
         targetSdk = 36
-        versionCode = 5
-        versionName = "0.2.0-qa3"
+        versionCode = 6
+        versionName = "1.0.0"
     }
 
     buildFeatures {
