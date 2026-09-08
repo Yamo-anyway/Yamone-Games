1f835ae3abddf11bda48929b6ddd283e2ee39885
Yamo <diahnet@naver.com>
2026-09-08 14:59:15 +0900

    Build arcade feature complete test version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yamone.games"
         minSdk = 26
         targetSdk = 36
-        versionCode = 10
-        versionName = "1.1.0-dev4"
+        versionCode = 11
+        versionName = "1.1.0-dev5"
     }
 
     buildFeatures {
