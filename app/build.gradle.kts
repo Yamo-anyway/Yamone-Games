f615f6a8580d463eb4b6afe2d5788e3283aa933d
Yamo <diahnet@naver.com>
2026-09-08 10:20:07 +0900

    Mark Sudoku build as QA candidate

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yamone.games"
         minSdk = 26
         targetSdk = 36
-        versionCode = 2
-        versionName = "0.2.0-dev1"
+        versionCode = 3
+        versionName = "0.2.0-qa1"
     }
 
     buildFeatures {
