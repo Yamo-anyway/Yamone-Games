20c5bfd5628d8e9f60ac00627eafbd5e69a3c0ca
Yamo <diahnet@naver.com>
2026-09-09 14:14:59 +0900

    Match system bar background to device theme

--- app/src/main/java/com/yamone/games/MainActivity.kt
+++ app/src/main/java/com/yamone/games/MainActivity.kt
@@ -3,6 +3,7 @@ package com.yamone.games
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.WindowInsets
 import androidx.compose.foundation.layout.WindowInsetsSides
@@ -10,11 +11,13 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.only
 import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.windowInsetsPadding
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import com.yamone.games.sudoku.ui.theme.YamoneSudokuTheme
 
 class MainActivity : ComponentActivity() {
@@ -25,11 +28,13 @@ class MainActivity : ComponentActivity() {
             var themeMode by remember { mutableStateOf(prefs.themeMode()) }
             var mascot by remember { mutableStateOf(prefs.mascot()) }
             var nickname by remember { mutableStateOf(prefs.nickname()) }
+            val systemBarBackground = if (isSystemInDarkTheme()) Color.Black else Color.White
 
             YamoneSudokuTheme(themeMode) {
                 Box(
                     Modifier
                         .fillMaxSize()
+                        .background(systemBarBackground)
                         .windowInsetsPadding(
                             WindowInsets.safeDrawing.only(WindowInsetsSides.Vertical)
                         )
