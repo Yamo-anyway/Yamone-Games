5e21d9a265d7962f196f0a40e5ddf174b9a98c2c
Yamo <diahnet@naver.com>
2026-09-09 15:52:53 +0900

    Add Winter Ride game module

---
+++ games/winterride/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.plugin.compose")
+}
+
+android {
+    namespace = "com.yamone.games.winterride"
+    compileSdk = 36
+
+    defaultConfig { minSdk = 26 }
+
+    buildFeatures { compose = true }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+}
+
+dependencies {
+    implementation("androidx.core:core-ktx:1.17.0")
+    implementation("androidx.compose.ui:ui:1.11.4")
+    implementation("androidx.compose.foundation:foundation:1.11.4")
+    implementation("androidx.compose.material3:material3:1.4.0")
+}
