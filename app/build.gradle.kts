2c3e538d398029133db21515dcd8f2b9e1cbbd95
Yamo <diahnet@naver.com>
2026-09-09 12:43:31 +0900

    Bump Yamone Games to v18

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yamone.games"
         minSdk = 26
         targetSdk = 36
-        versionCode = 17
-        versionName = "1.1.0-dev11"
+        versionCode = 18
+        versionName = "1.1.0-dev12"
     }
 
     signingConfigs {
