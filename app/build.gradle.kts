3cdbb9faca9af6df2de31fae703ae22f9ada32e2
Yamo <diahnet@naver.com>
2026-09-09 11:48:25 +0900

    Bump app version for ranking and arcade tuning

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yamone.games"
         minSdk = 26
         targetSdk = 36
-        versionCode = 14
-        versionName = "1.1.0-dev8"
+        versionCode = 15
+        versionName = "1.1.0-dev9"
     }
 
     buildFeatures {
