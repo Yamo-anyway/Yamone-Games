10693fa6b7a6327182afde7d6f32091af6d25c5e
Yamo <diahnet@naver.com>
2026-09-10 16:14:10 +0900

    Build winter ride visual upgrade validation APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yamone.games"
         minSdk = 26
         targetSdk = 36
-        versionCode = 46
-        versionName = "1.1.0-dev40"
+        versionCode = 47
+        versionName = "1.1.0-dev41"
     }
 
     signingConfigs {
