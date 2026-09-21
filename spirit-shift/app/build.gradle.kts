c61dc58a3175894c7a9616c4e2ae811524fa89b7
Yamo <diahnet@naver.com>
2026-09-21 17:27:27 +0900

    Raise Spirit Shift minSdk for current AdMob SDK

--- spirit-shift/app/build.gradle.kts
+++ spirit-shift/app/build.gradle.kts
@@ -9,7 +9,7 @@ android {
 
     defaultConfig {
         applicationId = "com.yamone.spiritshift"
-        minSdk = 23
+        minSdk = 24
         targetSdk = 36
         versionCode = 1
         versionName = "0.1.0"
